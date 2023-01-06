ed50047ac696230fce1afb4af65afcef10c608db
Mohamed.ElGohary <m.s.hafeez2291@gmail.com>
2023-01-06 14:14:06 +0200

    Initial commit

---
+++ src/main/java/com/springcloudcinfigserver/SpringCloudCinfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.springcloudcinfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCinfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudCinfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springcloudcinfigserver/SpringCloudCinfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloudcinfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudCinfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
